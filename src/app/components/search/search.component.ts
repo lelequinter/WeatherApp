c69bc5983db7cd605749a7f54be724272541d1ad
Leyder Quintero <lele.quinter@gmail.com>
2022-07-14 16:59:12 -0500

    trabajando sobre el Search Component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  template: `
+    <div class="search">
+      <input
+        class="search__input"
+        placeholder="City..."
+        [formControl]="inputSearch"
+      />
+    </div>
+  `,
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  inputSearch = new FormControl('');
+  @Output() submited = new EventEmitter<string>();
 
-  constructor() { }
+  // constructor() {}
 
   ngOnInit(): void {
+    this.onChange();
   }
 
+  private onChange(): void {
+    this.inputSearch.valueChanges
+      .pipe(
+        map((search: string) => search.trim()),
+        debounceTime(850),
+        distinctUntilChanged(),
+        filter((search: string) => search !== ''),
+        tap((search: string) => this.submited.emit(search))
+      )
+      .subscribe();
+  }
 }
