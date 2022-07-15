0bd7a9fe481384c372cd7f067a46e0d9c5d0ce54
Leyder Quintero <lele.quinter@gmail.com>
2022-07-15 15:09:03 -0500

    Añadimos la geoLocalizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { WeatherData } from '@shared/interfaces/weather.interface';
 import { WeatherService } from './pages/weather/services/weather.service';
+import { GeoLocationService } from './shared/services/geo-location.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,27 @@ import { WeatherService } from './pages/weather/services/weather.service';
 })
 export class AppComponent {
   public weather$!: Observable<WeatherData>;
-  constructor(private readonly weatherSvc: WeatherService) {}
+  constructor(
+    private readonly weatherSvc: WeatherService,
+    private readonly geoLocationSvc: GeoLocationService
+  ) {
+    if (navigator?.geolocation) {
+      this.getLocation();
+    }
+  }
 
   public onSearch(city: string): void {
     this.weather$ = this.weatherSvc.getWeatherByName(city);
     // this.weather$
+  }
+
+  private async getLocation(): Promise<void> {
+    try {
+      const { coords } = await this.geoLocationSvc.getCurrentPosition();
+      this.weather$ = this.weatherSvc.getWeatherByCoords(coords);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
